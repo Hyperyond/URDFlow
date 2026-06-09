595e320b475b8474fd1e698c24017b862130022c
Oliver Zhang <carolinscomakir68@gmail.com>
2026-06-10 00:39:48 +0800

    feat(urdf-web): re-export URDFRobot type for consumers

--- packages/urdf-web/src/index.ts
+++ packages/urdf-web/src/index.ts
@@ -2,3 +2,4 @@ export { loadURDFFromString, loadURDFFromURL } from "./loadURDF";
 export { getJointModel, setJoint } from "./joints";
 export { applyZUpToYUp } from "./coordinates";
 export type { JointInfo, URDFJointType, LoadURDFOptions } from "./types";
+export type { URDFRobot } from "urdf-loader";
