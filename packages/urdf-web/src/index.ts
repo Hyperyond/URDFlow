8ba2394bce01c468aed4e36602081e1281272afe
Oliver Zhang <carolinscomakir68@gmail.com>
2026-06-10 00:17:25 +0800

    feat(urdf-web): public API barrel

---
+++ packages/urdf-web/src/index.ts
@@ -0,0 +1,4 @@
+export { loadURDFFromString, loadURDFFromURL } from "./loadURDF";
+export { getJointModel, setJoint } from "./joints";
+export { applyZUpToYUp } from "./coordinates";
+export type { JointInfo, URDFJointType, LoadURDFOptions } from "./types";
